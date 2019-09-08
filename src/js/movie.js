dab170ff0e439e8826da37465b61bf6279f7bda9
Chris <cyeh@ucsd.edu>
2019-09-07 18:43:55 -0700

    First commit

---
+++ src/js/movie.js
@@ -0,0 +1,7 @@
+class Movie {
+    constructor() {
+        
+    }
+    String DataTransferItemList;    
+    // First match director, then genre and ratings, then cinematographer, then common actors and actresses
+}
